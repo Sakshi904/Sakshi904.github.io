d5a78e8a636073b88759ad4e631cfa0cf4301b05
sakshi gupta <sakshi.904@gmail.com>
2020-10-11 15:34:51 -0700

    Hookup portfolio creator

---
+++ src/resumeData.js
@@ -0,0 +1,103 @@
+let resumeData = {
+  imagebaseurl: "https://sakshi904.github.io/",
+  name: "Sakshi Gupta",
+  role: "Front End Developer and ServiceNow Technical Consultant",
+  linkedinId: "https://www.linkedin.com/in/sakshi-gupta-97233896/",
+  roleDescription: " I love technology",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/sakshi-gupta-97233896/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "https://github.com/Sakshi904",
+      className: "fa fa-github",
+    },
+  ],
+  aboutme: "About me",
+  address: "San Francisco Bay Area",
+  website: "https://sakshi904.github.io/",
+  education: [
+    {
+      UniversityName: "My University",
+      specialization: "Some specialization",
+      MonthOfPassing: "",
+      YearOfPassing: "",
+      Achievements: "Some Achievements",
+    },
+    {
+      UniversityName: "Some University",
+      specialization: "Some specialization",
+      MonthOfPassing: "Jan",
+      YearOfPassing: "2018",
+      Achievements: "Some Achievements",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "Some Company",
+      specialization: "Some specialization",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2018",
+      Achievements: "Some Achievements",
+    },
+    {
+      CompanyName: "Some Company",
+      specialization: "Some specialization",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2018",
+      Achievements: "Some Achievements",
+    },
+  ],
+  skillsDescription: "Your skills here",
+  skills: [
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS",
+    },
+    {
+      skillname: "Reactjs",
+    },
+    {
+      skillname: "ServiceNow",
+    },
+  ],
+  portfolio: [
+    {
+      name: "project1",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+    {
+      name: "project2",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project.jpg",
+    },
+    {
+      name: "project3",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project2.png",
+    },
+    {
+      name: "project4",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+  ],
+  testimonials: [
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+  ],
+};
+
+export default resumeData;
