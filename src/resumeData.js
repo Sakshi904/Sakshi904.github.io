ba8aae765158d084a32e1a89899ea2427731c25e
Sakshi904 <sakshi.904@gmail.com>
2020-10-12 19:51:57 -0700

    Updated resume

--- src/resumeData.js
+++ src/resumeData.js
@@ -3,10 +3,12 @@ let resumeData = {
   name: "Sakshi Gupta",
   role: "Front End Developer and ServiceNow Technical Consultant",
   linkedinId: "https://www.linkedin.com/in/sakshi-gupta-97233896/",
-  roleDescription: " I love technology",
+  servicenowProfile:
+    "https://account.servicenow.com/personal-data/11e9b491f/9177d7dad/b40a6cdff/a1f44NSQE/resume.html",
+  roleDescription: "",
   socialLinks: [
     {
-      name: "linkedin",
+      name: "LinkedIn",
       url: "https://www.linkedin.com/in/sakshi-gupta-97233896/",
       className: "fa fa-linkedin",
     },
@@ -16,39 +18,32 @@ let resumeData = {
       className: "fa fa-github",
     },
   ],
-  aboutme: "About me",
+  aboutme:
+    "I am a software developer with experience working in global teams. My hunger for knowledge and determination to turn information into action has contributed to me throughout my career. I believe mindfulness in the workplace is key to success - a tenet that I live out through my interests in yoga, meditation, gardening, and painting.",
   address: "San Francisco Bay Area",
   website: "https://sakshi904.github.io/",
+
   education: [
     {
-      UniversityName: "My University",
-      specialization: "Some specialization",
+      UniversityName: "Dr. A.P.J Abdul Kalaam University",
+      specialization:
+        "Bachelor's of Engineering and Technology in Computer Science",
       MonthOfPassing: "",
       YearOfPassing: "",
-      Achievements: "Some Achievements",
-    },
-    {
-      UniversityName: "Some University",
-      specialization: "Some specialization",
-      MonthOfPassing: "Jan",
-      YearOfPassing: "2018",
-      Achievements: "Some Achievements",
+      Achievements:
+        "Received award for scoring highest marks in Electrical Engineering course.",
     },
   ],
   work: [
     {
-      CompanyName: "Some Company",
-      specialization: "Some specialization",
-      MonthOfLeaving: "Jan",
+      CompanyName: "Tata Consultancy Services",
+      specialization: "Sotware Engineer",
+      MonthofStarting: "Aug",
+      YearOfStarting: "2015",
+      MonthOfLeaving: "Nov",
       YearOfLeaving: "2018",
-      Achievements: "Some Achievements",
-    },
-    {
-      CompanyName: "Some Company",
-      specialization: "Some specialization",
-      MonthOfLeaving: "Jan",
-      YearOfLeaving: "2018",
-      Achievements: "Some Achievements",
+      Achievements:
+        "On The Sport award for being an inspiring role model to the colleagues and in appreciation of the outstanding contribution to the organisation.",
     },
   ],
   skillsDescription: "Your skills here",
@@ -70,32 +65,47 @@ let resumeData = {
     {
       name: "Pomodero",
       description: "Pomodero Countdown Timer",
-      imgurl: "images/portfolio/coffee.jpg",
+      imgurl: "images/portfolio/Counter.png",
+      link: "https://pomoderocounter.herokuapp.com/",
+      code: "https://github.com/Sakshi904/Pomodero-Counter.git",
     },
     {
       name: "Random Image Gallery",
       description: "Search for Random images",
-      imgurl: "images/portfolio/console.jpg",
+      imgurl: "images/portfolio/Gallery.png",
+      link: "https://randomimagegallery.herokuapp.com",
+      code: "https://github.com/Sakshi904/RandomImageGallery.git",
     },
     {
       name: "React router tabs",
       description: "Tab based routing with ReactJS",
-      imgurl: "images/portfolio/origami.jpg",
+      imgurl: "images/portfolio/Tabs.png",
+      link: "https://tabsrouting.herokuapp.com",
+      code: "https://github.com/Sakshi904/Tabs-React-Router.git",
     },
     {
       name: "Rock Paper Scissors",
       description: "Chance based game using React hooks",
-      imgurl: "images/portfolio/judah.jpg",
+      imgurl: "images/portfolio/rpc.png",
+      link: "https://rockpaperscissor-game.herokuapp.com",
+      code: "https://github.com/Sakshi904/RockPaperScissor.git",
     },
   ],
   testimonials: [
     {
-      description: "This is a sample testimonial",
-      name: "Some technical guy",
+      description:
+        "Sakshi has been a brilliant developer at LafargeHolcim IT. Very hardworking, creative and resolutive.",
+      name: "Almudena Vera",
     },
     {
-      description: "This is a sample testimonial",
-      name: "Some technical guy",
+      description:
+        "Sakshi is skilled, hard-working and customer focussed. She strongly believes in Customer success and makes sure the features are delivered on-time and free of bugs.",
+      name: "Apoorv Mehta",
+    },
+    {
+      description:
+        "Sakshi is a hard working and smart resource. A keen learner and very well organized",
+      name: "Naziya Sahiba",
     },
   ],
 };
